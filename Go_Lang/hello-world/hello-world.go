7994fedc43be0838fcdfb470ece02c77bb88b329
Ram Polisetty Subbaiah <ramas@juniper.net>
2019-07-28 14:57:11 +0530

    First program

---
+++ Go_Lang/hello-world/hello-world.go
@@ -0,0 +1,19 @@
+/*
+ * hello-world.go
+ *
+ * My First Go Program, which prints "Hello World!!!" String.
+ *
+ * Author: Ram Polisetty
+ *
+ * How to run?
+ *
+ * $ go hello-world.go
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() {
+     fmt.Println("Hello World!!!")
+ }
